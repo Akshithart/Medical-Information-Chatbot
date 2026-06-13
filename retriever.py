6f6a22494af9f225a04074e96eb266b1eb27ade4
Akshithart <rtakshi365@gmail.com>
2026-06-13 23:20:54 +0530

    retriver update

--- retriever.py
+++ retriever.py
@@ -1,1 +1,31 @@
  
+import faiss
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer(
+    "all-MiniLM-L6-v2"
+)
+
+index = faiss.read_index(
+    "vectorstore/faiss.index"
+)
+
+def retrieve(query,chunks):
+
+    query_vector=model.encode(
+        [query]
+    )
+
+    D,I=index.search(
+        query_vector,
+        3
+    )
+
+    result=[]
+
+    for idx in I[0]:
+        result.append(
+            chunks[idx]
+        )
+
+    return result
